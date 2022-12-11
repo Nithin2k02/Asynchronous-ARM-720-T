8dab5e9910f3fbc18328f77f88196168d1ba88db
anisari10 <94749249+anisari10@users.noreply.github.com>
2022-12-11 10:54:10 +0530

    Update tb_reg_sync.v
    working for delay in writing only

--- tb_reg_sync.v
+++ tb_reg_sync.v
@@ -1,8 +1,11 @@
+`timescale 1ns / 1ps
+
 module register_test;
 
 reg[3:0] write_address_1;
 reg[3:0] write_address_2;
 reg[3:0] write_address_3;
+reg[3:0] write_address_4;
 
 reg[3:0] in_address_1;
 reg[3:0] in_address_2;
@@ -13,10 +16,12 @@ reg[3:0] in_address_3;
 reg[31:0] write_data_1;
 reg[31:0] write_data_2;
 reg[31:0] write_data_3;
+reg[31:0] write_data_4;
 reg write_enable_1;
 reg write_enable_2;
 reg write_enable_3;
-
+reg write_enable_4;
+ 
 
 reg[31:0] pc_update;
 reg pc_write;
@@ -44,12 +49,15 @@ reg_sync RegisterFile(	//inputs
 							.write_address_1(write_address_1),
 							.write_address_2(write_address_2),
 							.write_address_3(write_address_3),
+							.write_address_4(write_address_4),
 							.write_data_1(write_data_1),
 							.write_data_2(write_data_2),
 							.write_data_3(write_data_3),
+							.write_data_4(write_data_4),
 							.write_enable_1(write_enable_1),
 							.write_enable_2(write_enable_2),
 							.write_enable_3(write_enable_3),
+							.write_enable_4(write_enable_4),
 							
 							.pc_update(pc_update), 
 							.pc_write(pc_write), 
@@ -92,23 +100,24 @@ initial begin
 	write_address_1 = 0;
 	write_address_2 = 0;
 	write_address_3 = 0;
+	write_address_4 = 0;
 	
-	#2 
+	#2 //[0,10) => delay of 10ns; [10,20) => delay of 20ns
 	write_address_1 = 4'b0000;
     write_enable_1 = 1;
     write_data_1 = 32'h00000002;
 
-	#10 // delays should be multiples of 10 (time period of clk)
+	#20 // 
 	write_address_2 = 4'b0001;
 	write_enable_2 = 1;
-	write_data_2 = 32'h00000002;
+	write_data_2 = 32'h00000001;
 
 	//$display("pc %h", pc);
-	#10
-	Rs = out_data_1;
-	Rm = out_data_2;
+	#20
+	Rs = out_data_1; 
+	Rm = out_data_2; 
 	
-	repeat(10)
+	repeat(30)  
 	begin
 	#5
 	write_address_3 = 4'b0010; //result address on address lines of reg
@@ -116,15 +125,15 @@ initial begin
 	write_data_3 = result;	//write result in that reg
 	//write_enable_3 = 0;	//indicates writing is done
 	#15
-	Rs = out_data_3 //result placed on bus; given to multiplier
+	Rm = out_data_3; //result placed on bus; given to multiplier
 	end
 
 	#5
-	write_address_1 = 4'b0011;
-	write_enable_1 = 1;
-	write_data_1 = result;
+	write_address_4 = 4'b0011;
+	write_enable_4 = 1; 
+	write_data_4 = result;
 	//$display("pc %h", pc);
-end
+end 
 
 
 endmodule
