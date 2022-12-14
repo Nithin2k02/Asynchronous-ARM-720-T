e1fc04e16d8ada4d3ba5dad1e9cca554f40b9661
anisari10 <94749249+anisari10@users.noreply.github.com>
2022-12-14 16:07:05 +0530

    Update tb_reg_async.v
    automatic delay sensing with no delay independance.

--- tb_reg_async.v
+++ tb_reg_async.v
@@ -1,8 +1,29 @@
-module reg_mult_tb;
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:13:54 12/13/2022 
+// Design Name: 
+// Module Name:    tb_reg_async_2 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module tb_reg_async_2;
 
 reg[3:0] write_address_1;
 reg[3:0] write_address_2;
 reg[3:0] write_address_3;
+reg[3:0] write_address_4;
 
 reg[3:0] in_address_1;
 reg[3:0] in_address_2;
@@ -14,12 +35,15 @@ reg[3:0] in_address_3;
 reg[31:0] write_data_1;
 reg[31:0] write_data_2;
 reg[31:0] write_data_3;
+reg[31:0] write_data_4;
 reg write_enable_1;
 reg write_enable_2;
 reg write_enable_3;
+reg write_enable_4;
 reg read_enable_1;
 reg read_enable_2;
 reg read_enable_3;
+
 
 reg[31:0] pc_update;
 reg pc_write;
@@ -39,7 +63,7 @@ reg[31:0] Rs;
 wire[31:0] result;
 
 
-register_file RegisterFile(	//inputs
+reg_async RegisterFile(	//inputs
 							.in_address_1(in_address_1),
 							.in_address_2(in_address_2),
 							.in_address_3(in_address_3),
@@ -48,12 +72,15 @@ register_file RegisterFile(	//inputs
 							
 							.write_address_1(write_address_1),
 							.write_address_2(write_address_2),
+							.write_address_3(write_address_3),
+							.write_address_4(write_address_4),
 							.write_data_1(write_data_1),
 							.write_data_2(write_data_2),
 							.write_enable_1(write_enable_1),
 							.write_enable_2(write_enable_2),
-							.write_data_3(write_data_3).
-							.write_enable_3(write_enable_3)
+							.write_data_3(write_data_3),
+							.write_enable_3(write_enable_3),
+							.write_enable_4(write_enable_4),
 							.read_enable_1(read_enable_1),
 							.read_enable_2(read_enable_2),
 							.read_enable_3(read_enable_3),
@@ -76,79 +103,95 @@ register_file RegisterFile(	//inputs
 							
 							);
 
-async_clk async_clk(.clk(clk), .req(req), .ack(ack));
+async_clk clock(.clk(clk), .req(req), .ack(ack));
 multiplier mult(.Rs(Rs), .Rm(Rm), .result(result));
 
-always @(*)
-if(ack == 1)
-begin
-write_enable_1 = 0;
-write_enable_2 = 0;
-write_enable_3 = 0;
-read_enable_1 = 0;
-read_enable_2 = 0;
-read_enable_3 = 0;
-end
+//always@ (ack) begin
+//write_enable_1 = 0;
+//write_enable_2 = 0;
+//write_enable_3 = 0;
+//write_enable_4 = 0;
+//read_enable_1 = 0;
+//read_enable_2 = 0;
+//read_enable_3 = 0;
+//end
+
 
 initial begin
-	#2 
-	// place address of reg 0 on address lines
+	while (ack == 0)
+	begin #1; end
+	write_enable_1 = 1;
 	write_address_1 = 4'b0000;
-	// enable register to ask for req
-    write_enable_1 = 1;
-	// by the time reg's req gets served, data is placed on the data lines 
-    write_data_1 = 32'h00000002;
-
-	// it takes 6ns for data to get into register
-
-	#8
-	// place address of reg 1 on address lines
-	write_address_2 = 4'b0001;
-	// enable reg to ask for req
-	write_enable_2 = 1;
-	// by the time reg's req gets served, data is placed on the data lines
-	write_data_2 = 32'h00000002;
+	write_data_1 = 32'h2;
+	#2;
 	
-	// it takes 6ns for data to get into register
-
-	#8
-	// read_enable_1 is set to allow reading of data in reg_0
+	while (ack == 0)
+	begin #1; end
+	write_enable_1 = 0;
+//	#6
+	write_enable_2 = 1;
+	write_address_2 = 4'b0001;
+	write_data_2 = 32'h2;
+	#2
+	
+	while (ack == 0) 
+	begin #1; end
+	
+	write_enable_2 = 0;
+//	#6
 	read_enable_1 = 1;
 	in_address_1 = write_address_1;
-	#8
-	// 1st operand is on data lines
-	Rs = out_data_1;
-	// read_enable_2 is set to allow reading of data in reg_1
+	#2
+	while (ack == 0)
+	begin #1; end
+	
+	read_enable_1 = 0;
+//	#6
+	Rm = out_data_1; 
+	
+	while (ack == 0)
+	begin #1; end
+//	#6
 	read_enable_2 = 1;
 	in_address_2 = write_address_2;
-	#8
-	// 2nd operand is on data lines
-	Rm = out_data_2;
-
-	// Rs = out_data_1;
-	// Rm = out_data_2;
-
-	// repeat multiplication 10 times
-	repeat(10)
-	begin
 	#2
-	write_address_3 = 4'b0010;
+	while (ack == 0)
+	begin #1; end
+	read_enable_2 = 0;
+//	#6
+	Rs = out_data_2; 
+	
+	repeat(29) begin
+	#2
+	while (ack == 0)
+	begin #1; end
 	write_enable_3 = 1;
+	write_address_3 = 4'b0010;
 	write_data_3 = result;
-	#8
-	// temp_result is stored in reg_2
-	// now temp_result has to be retrieved back to multiplier
+	#2
+	while (ack == 0)
+	begin #1; end
+	write_enable_3 = 0;
+//	#6
 	read_enable_3 = 1;
 	in_address_3 = write_address_3;
-	#8
-	// temp_result is on data lines
-	Rs = out_data_3;
-	end
+	#2
+	while (ack == 0)
+	begin #1; end
+	read_enable_3 = 0;
+//	#6
+	Rs = out_data_3; 	
 
-	#5
-	write_address_1 = 4'b0011;
-	write_enable_1 = 1;
-	write_data_1 = result;
+	end
+	
+	write_enable_4 = 1;
+	write_address_4 = 4'b0011;
+	#2
+	write_data_4 = result;
+	while (ack == 0)
+	begin #1; end
+	write_enable_4 = 0;
+
 end
 
 
